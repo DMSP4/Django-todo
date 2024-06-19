23a1b148ea61e36a4b4d70b032bb5dfb381826fe
Max <max.prokopchuk@gmail.com>
2024-06-19 17:23:53 +0300

    chore: Register Todo model in admin.py

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
 
     path('current/', views.currenttodos, name='currenttodos'),
