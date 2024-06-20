e1c315b7cc8c13203b93e272cf8ff88323ef013b
Max <max.prokopchuk@gmail.com>
2024-06-20 14:34:00 +0300

    chore: Update templates and views for completed todos functionality

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
     path('logout/', views.logoutuser, name='logoutuser'),
 
     path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
     path('create/', views.createtodo, name='createtodo'),
     path('', views.home, name='home'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
